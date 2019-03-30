0a2d351afadee37070454b29c8e6e96d12f9f634
Catbox <catboxlee@gmail.com>
2019-03-30 15:26:18 +0800

    t

--- common/cmd.go
+++ common/cmd.go
@@ -4,7 +4,9 @@ import (
 	"regexp"
 	"strings"
 	"strconv"
+	"fmt"
 	"myBot/world"
+	"myBot/emoji"
 )
 
 // Cmd ...
@@ -22,6 +24,8 @@ func Cmd(input string) []string {
 			switch matches[1] {
 				case "game":
 					texts = changeGame(matches[2])
+				case "bank":
+					texts = setBank(matches[2])
 			}
 		}
 		return texts
@@ -43,4 +47,20 @@ func changeGame(s string) []string{
 		}
 	}
 	return []string{"[1]終極密碼\n[2]射龍門"}
+}
+func setBank(s string) []string{
+	re := regexp.MustCompile(`(^\w+)\s?(.*)`)
+	matches := re.FindStringSubmatch(s)
+	if len(matches) > 1 {
+		switch matches[1] {
+		case "set":
+			if n, err := strconv.Atoi(matches[2]); err == nil {
+				world.World.Bank = n
+				world.World.SaveWorldData()
+				pokergoal.Pokergoal.pot = n
+				return []string{"ok"}
+			}
+		}
+	}
+	return []string{fmt.Sprintf("%s 銀行\n[1]借款\n[2]還款", emoji.Emoji(":bank:"))}
 }
