782a3fae4dffbf1eb6c0dccfce0057fcfa796b92
Catbox <catboxlee@gmail.com>
2019-03-30 13:26:58 +0800

    o

--- games/pokergoal/pokergoal.go
+++ games/pokergoal/pokergoal.go
@@ -106,7 +106,7 @@ func (p *gameType) Run(input string) []string {
 			p.showPot()
 			text = fmt.Sprintf("%s 下注：%s %d\n", users.LineUser.UserProfile.DisplayName, emoji.Emoji(":money_bag:"), p.antes)
 			text += p.dealGate(currentPlayer)
-			text += fmt.Sprintf("\n剩餘資金：%d",  users.UsersList.Data[users.LineUser.UserProfile.UserID].Money)
+			text += fmt.Sprintf("\n剩餘資金：%s %d", emoji.Emoji(":money_bag:"),  users.UsersList.Data[users.LineUser.UserProfile.UserID].Money)
 			if len(currentPlayer.cards) == 2 {
 				text += fmt.Sprintf("\n可加注：+%d ~ +%d (預設+0)", 0, helper.Min(users.UsersList.Data[users.LineUser.UserProfile.UserID].Money, p.pot))
 			}
