bf18fad74eaf5a1f940bedf5ba8187331a9348ff
Catbox <catboxlee@gmail.com>
2020-12-11 22:08:34 +0800

    .

--- games/boomgame/scene4.go
+++ games/boomgame/scene4.go
@@ -194,8 +194,8 @@ func (b *scene4InfoType) chkChance(g *GameType) string {
 		}
 	default:
 		strs = fmt.Sprintf("【%s】 Shielder瑪修「頌為堅城的雪花之壁！」", g.data.players.List[b.Info["CurrentPlayerID"].(string)].DisplayName)
-		if b.Info["BoomCnt"] > 99 {
-			b.Info["BoomCnt"] = 99
+		if b.Info["BoomCnt"].(float64) > float64(99) {
+			b.Info["BoomCnt"] = float64(99)
 		} else {
 			b.Info["BoomCnt"] = math.Ceil(b.Info["BoomCnt"].(float64) / 3)
 		}
