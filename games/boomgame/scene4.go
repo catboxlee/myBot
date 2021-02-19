c809757d89f07dd3db56e20b595bd027c01dd01c
catboxlee <9l.catbox@gmail.com>
2021-02-19 09:14:47 +0800

    Add files via upload

--- games/boomgame/scene4.go
+++ games/boomgame/scene4.go
@@ -117,24 +117,25 @@ func (b *scene4InfoType) gameOver(g *GameType) {
 func (b *scene4InfoType) chkChance(g *GameType) string {
 	boomDice := &dice.Dice
 	var strs string
-	if g.data.players.List[b.Info["CurrentPlayerID"].(string)].SwallowReturn > 0 {
+	var swallowReturn = users.UsersList.Data[b.Info["CurrentPlayerID"].(string)].SwallowReturn + g.data.players.List[b.Info["CurrentPlayerID"].(string)].SwallowReturn
+	if swallowReturn > 0 {
 		if len(b.Info["LastPlayerID"].(string)) > 0 {
 			boomDice.Roll("1d100")
-			if g.data.players.List[b.Info["CurrentPlayerID"].(string)].SwallowReturn >= int(boomDice.Hit) {
-				strs += fmt.Sprintf("【%s】「燕返%d%%！」發動!\n", g.data.players.List[b.Info["CurrentPlayerID"].(string)].DisplayName, g.data.players.List[b.Info["CurrentPlayerID"].(string)].SwallowReturn)
+			if swallowReturn >= int(boomDice.Hit) {
+				strs += fmt.Sprintf("【%s】「燕返%d%%！」發動!\n", g.data.players.List[b.Info["CurrentPlayerID"].(string)].DisplayName, swallowReturn)
 				tmp := g.data.players.List[b.Info["CurrentPlayerID"].(string)]
 				if users.UsersList.Data[b.Info["CurrentPlayerID"].(string)].SwallowReturn <= 30 {
 					users.UsersList.Data[b.Info["CurrentPlayerID"].(string)].SwallowReturn += 2
 					strs += fmt.Sprintf("【%s】獲得 常註燕返%d%%(+2%%)！\n", g.data.players.List[b.Info["CurrentPlayerID"].(string)].DisplayName, users.UsersList.Data[b.Info["CurrentPlayerID"].(string)].SwallowReturn)
 				}
-				tmp.SwallowReturn = users.UsersList.Data[b.Info["CurrentPlayerID"].(string)].SwallowReturn
+				tmp.SwallowReturn = 0
 				g.data.players.List[b.Info["CurrentPlayerID"].(string)] = tmp
 				b.Info["LastPlayerID"], b.Info["CurrentPlayerID"] = b.Info["CurrentPlayerID"], b.Info["LastPlayerID"]
 				strs += fmt.Sprintf("%s%s", strs, b.chkChance(g))
 			} else {
-				strs += fmt.Sprintf("【%s】「燕返%d%%！」失敗.\n", g.data.players.List[b.Info["CurrentPlayerID"].(string)].DisplayName, g.data.players.List[b.Info["CurrentPlayerID"].(string)].SwallowReturn)
+				strs += fmt.Sprintf("【%s】「燕返%d%%！」失敗.\n", g.data.players.List[b.Info["CurrentPlayerID"].(string)].DisplayName, swallowReturn)
 				tmp := g.data.players.List[b.Info["CurrentPlayerID"].(string)]
-				tmp.SwallowReturn = users.UsersList.Data[b.Info["CurrentPlayerID"].(string)].SwallowReturn
+				tmp.SwallowReturn = 0
 				g.data.players.List[b.Info["CurrentPlayerID"].(string)] = tmp
 			}
 		}
@@ -161,7 +162,7 @@ func (b *scene4InfoType) chkFate(g *GameType) {
 		boomDice.Roll("1d10")
 		tmp.SwallowReturn += boomDice.Hit
 		g.data.players.List[b.Info["CurrentPlayerID"].(string)] = tmp
-		texts = append(texts, fmt.Sprintf("【%s】獲得 燕返%d%%(+%d%%)", g.data.players.List[b.Info["CurrentPlayerID"].(string)].DisplayName, g.data.players.List[b.Info["CurrentPlayerID"].(string)].SwallowReturn, boomDice.Hit))
+		texts = append(texts, fmt.Sprintf("【%s】獲得 燕返%d%%(+%d%%)", g.data.players.List[b.Info["CurrentPlayerID"].(string)].DisplayName, users.UsersList.Data[b.Info["CurrentPlayerID"].(string)].SwallowReturn+g.data.players.List[b.Info["CurrentPlayerID"].(string)].SwallowReturn, boomDice.Hit))
 	}
 
 	if 3 == int(b.Info["Max"].(float64)-b.Info["Min"].(float64)) {
